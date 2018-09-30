467db8f424ab503ee49994e11caea7b915e1e52d
Tim James <32233010+tjames222@users.noreply.github.com>
2018-09-29 19:28:50 -0700

    Tic-Tac-Toe

---
+++ Programming Project 4/tic tac toe/tic tac toe/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label00 = new System.Windows.Forms.Label();
+            this.label01 = new System.Windows.Forms.Label();
+            this.label02 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.buttonNG = new System.Windows.Forms.Button();
+            this.labelWinner = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonExit.FlatAppearance.BorderSize = 2;
+            this.buttonExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.buttonExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonExit.Location = new System.Drawing.Point(198, 417);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(162, 37);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "EXIT";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.ButtonExit_Click);
+            // 
+            // label00
+            // 
+            this.label00.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label00.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label00.Font = new System.Drawing.Font("Matura MT Script Capitals", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label00.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label00.Location = new System.Drawing.Point(24, 23);
+            this.label00.Name = "label00";
+            this.label00.Size = new System.Drawing.Size(108, 103);
+            this.label00.TabIndex = 0;
+            this.label00.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label00.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label01
+            // 
+            this.label01.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label01.Font = new System.Drawing.Font("Matura MT Script Capitals", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label01.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label01.Location = new System.Drawing.Point(138, 23);
+            this.label01.Name = "label01";
+            this.label01.Size = new System.Drawing.Size(108, 103);
+            this.label01.TabIndex = 2;
+            this.label01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label02
+            // 
+            this.label02.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label02.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label02.Font = new System.Drawing.Font("Matura MT Script Capitals", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label02.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label02.Location = new System.Drawing.Point(252, 23);
+            this.label02.Name = "label02";
+            this.label02.Size = new System.Drawing.Size(108, 103);
+            this.label02.TabIndex = 3;
+            this.label02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Font = new System.Drawing.Font("Matura MT Script Capitals", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label10.Location = new System.Drawing.Point(24, 135);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(108, 103);
+            this.label10.TabIndex = 4;
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Font = new System.Drawing.Font("Matura MT Script Capitals", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label11.Location = new System.Drawing.Point(138, 135);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(108, 103);
+            this.label11.TabIndex = 5;
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label12.Font = new System.Drawing.Font("Matura MT Script Capitals", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label12.Location = new System.Drawing.Point(252, 135);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(108, 103);
+            this.label12.TabIndex = 6;
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label20
+            // 
+            this.label20.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label20.Font = new System.Drawing.Font("Matura MT Script Capitals", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label20.Location = new System.Drawing.Point(24, 247);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(108, 103);
+            this.label20.TabIndex = 7;
+            this.label20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label21
+            // 
+            this.label21.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label21.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label21.Font = new System.Drawing.Font("Matura MT Script Capitals", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label21.Location = new System.Drawing.Point(138, 247);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(108, 103);
+            this.label21.TabIndex = 8;
+            this.label21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label22
+            // 
+            this.label22.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label22.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label22.Font = new System.Drawing.Font("Matura MT Script Capitals", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label22.Location = new System.Drawing.Point(252, 247);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(108, 103);
+            this.label22.TabIndex = 9;
+            this.label22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonNG
+            // 
+            this.buttonNG.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonNG.FlatAppearance.BorderSize = 2;
+            this.buttonNG.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.buttonNG.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.buttonNG.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNG.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNG.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonNG.Location = new System.Drawing.Point(24, 417);
+            this.buttonNG.Name = "buttonNG";
+            this.buttonNG.Size = new System.Drawing.Size(162, 37);
+            this.buttonNG.TabIndex = 10;
+            this.buttonNG.Text = "NEW GAME";
+            this.buttonNG.UseVisualStyleBackColor = true;
+            this.buttonNG.Click += new System.EventHandler(this.ButtonNG_Click);
+            // 
+            // labelWinner
+            // 
+            this.labelWinner.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelWinner.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWinner.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelWinner.Location = new System.Drawing.Point(24, 367);
+            this.labelWinner.Name = "labelWinner";
+            this.labelWinner.Size = new System.Drawing.Size(336, 35);
+            this.labelWinner.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(382, 477);
+            this.Controls.Add(this.labelWinner);
+            this.Controls.Add(this.buttonNG);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label02);
+            this.Controls.Add(this.label01);
+            this.Controls.Add(this.label00);
+            this.Controls.Add(this.buttonExit);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label00;
+        private System.Windows.Forms.Label label01;
+        private System.Windows.Forms.Label label02;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Button buttonNG;
+        private System.Windows.Forms.Label labelWinner;
+    }
+}
+
---
+++ Programming Project 4/tic tac toe/tic tac toe/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+
+        // Random number generator
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonNG_Click(object sender, EventArgs e)
+        {
+
+            // Clear text box
+            labelWinner.Text = "";
+
+            const string x = "x";
+            const string o = "o";
+
+            // Declare constants for columns and rows
+            const int ROWS = 3;
+            const int COLS = 3;
+
+            // Declare two dimensional array
+            int[,] labels = new int[ROWS, COLS];
+
+            // Fill the Array with random numbers
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLS; col++)
+                {
+                    labels[row, col] = rand.Next(2);
+                }
+            }
+
+            // Display array contents
+            // Change number to X or O
+            if (labels[0,0] == 0)
+            {
+                label00.Text = x;
+            }
+            else
+            {
+                label00.Text = o;
+            }
+
+            if (labels[0, 1] == 0)
+            {
+                label01.Text = x;
+            }
+            else
+            {
+                label01.Text = o;
+            }
+
+            if (labels[0, 2] == 0)
+            {
+                label02.Text = x;
+            }
+            else
+            {
+                label02.Text = o;
+            }
+
+            if (labels[1, 0] == 0)
+            {
+                label10.Text = x;
+            }
+            else
+            {
+                label10.Text = o;
+            }
+
+            if (labels[1, 1] == 0)
+            {
+                label11.Text = x;
+            }
+            else
+            {
+                label11.Text = o;
+            }
+
+            if (labels[1, 2] == 0)
+            {
+                label12.Text = x;
+            }
+            else
+            {
+                label12.Text = o;
+            }
+
+            if (labels[2, 0] == 0)
+            {
+                label20.Text = x;
+            }
+            else
+            {
+                label20.Text = o;
+            }
+
+            if (labels[2, 1] == 0)
+            {
+                label21.Text = x;
+            }
+            else
+            {
+                label21.Text = o;
+            }
+
+            if (labels[2, 2] == 0)
+            {
+                label22.Text = x;
+            }
+            else
+            {
+                label22.Text = o;
+            }
+
+            // Display Winner
+            if (x == label00.Text && x == label01.Text && x == label02.Text || x == label10.Text && x == label11.Text && x == label12.Text ||
+                x == label20.Text && x == label21.Text && x == label22.Text || x == label20.Text && x == label11.Text && x == label02.Text ||
+                x == label00.Text && x == label11.Text && x == label22.Text || x == label00.Text && x == label10.Text && x == label20.Text ||
+                x == label01.Text && x == label11.Text && x == label21.Text || x == label02.Text && x == label12.Text && x == label22.Text) 
+            {
+                labelWinner.Text = "PLAYER X IS THE WINNER!";
+            }
+            else if (o == label00.Text && o == label01.Text && o == label02.Text || o == label10.Text && o == label11.Text && o == label12.Text ||
+                o == label20.Text && o == label21.Text && o == label22.Text || o == label20.Text && o == label11.Text && o == label02.Text ||
+                o == label00.Text && o == label11.Text && o == label22.Text || o == label00.Text && o == label10.Text && o == label20.Text ||
+                o == label01.Text && o == label11.Text && o == label21.Text || o == label02.Text && o == label12.Text && o == label22.Text)
+            {
+                labelWinner.Text = "PLAYER O IS THE WINNER!";
+            }
+            else
+            {
+                labelWinner.Text = "CATS GAME!!";
+            }
+        }
+
+        private void ButtonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
